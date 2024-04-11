255585a1462cb776e29a2b66e4631edea2173095
lashlyn <vat.13@list.ru>
2024-04-11 15:47:25 +0700

    add spm support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "openai-assistants-api",
+    platforms: [
+        .iOS(.v16),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "openai-assistants-api",
+            targets: ["openai-assistants-api"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "openai-assistants-api",
+            dependencies: [
+                .product(name: "CombineMoya", package: "Moya")],
+            path: "OpenAIAssistants/Source")
+    ]
+)
